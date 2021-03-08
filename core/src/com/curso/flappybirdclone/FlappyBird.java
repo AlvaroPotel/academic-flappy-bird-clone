4e1670822d9e01347ffb2ad62438d05143894b32
alvaropotel <alvaropotel@gmail.com>
2021-03-08 15:52:49 +0000

    Animating Bird

--- core/src/com/curso/flappybirdclone/FlappyBird.java
+++ core/src/com/curso/flappybirdclone/FlappyBird.java
@@ -8,35 +8,51 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
 
-	SpriteBatch batch;
-	Texture bird;
+	private SpriteBatch batch;
+	private Texture[] birds;
 	private Texture background;
 
 	//configuration attributes
 	private int deviceWidth;
 	private int deviceHeight;
-	private int moving = 0;
+
+	private float variable = 0;
+
+	private float velocityFall = 0;
+	private float verticalStartingPosition;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		bird = new Texture("passaro1.png");
+		birds = new Texture[3];
+		birds[0] = new Texture("passaro1.png");
+		birds[1] = new Texture("passaro2.png");
+		birds[2] = new Texture("passaro3.png");
 		background = new Texture("fundo.png");
+
 		deviceWidth = Gdx.graphics.getWidth();
 		deviceHeight = Gdx.graphics.getHeight();
+		verticalStartingPosition = deviceHeight/2;
 
 
 	}
 
 	@Override
 	public void render () {
-		//Gdx.gl.glClearColor(1, 0, 0, 1);
-		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		moving++;
+
+		variable += Gdx.graphics.getDeltaTime()*5;
+		velocityFall ++;
+
+		if(variable>2) variable = 0;
+
+		if(verticalStartingPosition > 0){
+			verticalStartingPosition = verticalStartingPosition - velocityFall;
+		}
+
 
 		batch.draw(background, 0,0,deviceWidth,deviceHeight);
-		batch.draw(bird, moving, 700);
+		batch.draw(birds[(int)variable], 30, verticalStartingPosition);
 		batch.end();
 
 
