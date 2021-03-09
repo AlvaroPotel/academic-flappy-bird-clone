9cd351946ee2c8d7a6b30326c5440fcc3a011e9a
alvaropotel <alvaropotel@gmail.com>
2021-03-09 10:48:00 +0000

    Add pipes

--- core/src/com/curso/flappybirdclone/FlappyBird.java
+++ core/src/com/curso/flappybirdclone/FlappyBird.java
@@ -6,11 +6,15 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class FlappyBird extends ApplicationAdapter {
 
 	private SpriteBatch batch;
 	private Texture[] birds;
 	private Texture background;
+	private Texture pipeDown;
+	private Texture pipeUp;
 
 	//configuration attributes
 	private int deviceWidth;
@@ -21,6 +25,15 @@ public class FlappyBird extends ApplicationAdapter {
 	private float velocityFall = 0;
 	private float verticalStartingPosition;
 
+	//configuration pipes
+	private float pipeMovingPositionHorizontal;
+	private float pipeMovingPositionVertical;
+	private float roomBtwPipes;
+	private Random numberRandom;
+	private float heightPipesRandom;;
+
+	private float deltaTime;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -28,11 +41,19 @@ public class FlappyBird extends ApplicationAdapter {
 		birds[0] = new Texture("passaro1.png");
 		birds[1] = new Texture("passaro2.png");
 		birds[2] = new Texture("passaro3.png");
+
 		background = new Texture("fundo.png");
+
+		pipeDown = new Texture("cano_baixo_maior.png");
+		pipeUp = new Texture("cano_topo_maior.png");
 
 		deviceWidth = Gdx.graphics.getWidth();
 		deviceHeight = Gdx.graphics.getHeight();
 		verticalStartingPosition = deviceHeight/2;
+		pipeMovingPositionHorizontal = deviceWidth;
+		pipeMovingPositionVertical = deviceWidth;
+		roomBtwPipes = 300;
+		numberRandom = new Random();
 
 
 	}
@@ -41,18 +62,34 @@ public class FlappyBird extends ApplicationAdapter {
 	public void render () {
 		batch.begin();
 
-		variable += Gdx.graphics.getDeltaTime()*5;
+		deltaTime = Gdx.graphics.getDeltaTime();
+
+		variable += deltaTime *5;
+		pipeMovingPositionHorizontal -= deltaTime *200;
 		velocityFall ++;
 
 		if(variable>2) variable = 0;
 
-		if(verticalStartingPosition > 0){
+		if(Gdx.input.justTouched()){
+			velocityFall = - 15;
+		}
+
+		if(verticalStartingPosition > 0 || velocityFall < 0){
 			verticalStartingPosition = verticalStartingPosition - velocityFall;
+		}
+
+		//checks if the pipe has left the screen
+		if(pipeMovingPositionHorizontal < -pipeUp.getWidth()){
+			pipeMovingPositionHorizontal = deviceWidth;
+			heightPipesRandom = numberRandom.nextInt(400) - 200;
 		}
 
 
 		batch.draw(background, 0,0,deviceWidth,deviceHeight);
-		batch.draw(birds[(int)variable], 30, verticalStartingPosition);
+		batch.draw(pipeUp, pipeMovingPositionHorizontal,deviceHeight / 2 + roomBtwPipes/2 + heightPipesRandom);
+		batch.draw(pipeDown, pipeMovingPositionHorizontal,deviceHeight / 2 - pipeDown.getHeight() - roomBtwPipes/2 + heightPipesRandom);
+		batch.draw(birds[(int)variable], 120, verticalStartingPosition);
+
 		batch.end();
 
 
